f09aa9ef6b184ca40b3273bdf7b81488926f54f6
tabi5 <tinbitdemissie@gmail.com>
2024-01-16 08:28:46 -0800

    poi

--- models/amenity.py
+++ models/amenity.py
@@ -1,7 +1,25 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the Amenity class"""
+
+# Import necessary modules and classes
+from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String
+from models.place import place_amenity
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    """This is the class for Amenity
+    Attributes:
+        name: input name
+    """
+
+    # Define the table name for the class in the database
+    __tablename__ = "amenities"
+
+    # Define the columns of the table
+    name = Column(String(128), nullable=False)
+
+    # Define the relationship with the Place class through
+    # the place_amenity association table
+    place_amenities = relationship("Place", secondary=place_amenity)
--- models/city.py
+++ models/city.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
-""" City Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the city class"""
+from sqlalchemy.ext.declarative import declarative_base
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from models.place import Place
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+class City(BaseModel, Base):
+    """This is the class for City.
+
+    Attributes:
+        state_id (str): The ID of the state associated with the city.
+        name (str): The name of the city.
+        places (relationship): A relationship to the Place model,
+        representing the places in the city.
+    """
+    __tablename__ = "cities"
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship("Place", cascade='all, delete, delete-orphan',
+                          backref="cities")
