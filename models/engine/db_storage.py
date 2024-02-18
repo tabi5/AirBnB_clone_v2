6dbf21ff0c4aa3703b08966c32a3895d041b1b1d
tabi5 <tinbitdemissie@gmail.com>
2024-02-18 08:13:22 -0800

    model

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,268 +1,76 @@
 #!/usr/bin/python3
-""" new class for sqlAlchemy """
-from os import getenv
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import Base
-from models.state import State
+"""
+Contains the class DBStorage
+"""
+
+import models
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
 from models.city import City
-from models.user import User
 from models.place import Place
 from models.review import Review
-from models.amenity import Amenity
+from models.state import State
+from models.user import User
+from os import getenv
+import sqlalchemy
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class DBStorage:
-    """create tables in environmental"""
-
+    """interaacts with the MySQL database"""
     __engine = None
     __session = None
 
     def __init__(self):
-        """
-        Initialize DBStorage instance by creating engine and session.
-
-        Parameters:
-            None
-
-        Returns:
-            None
-
-        Description:
-            This method initializes an instance of the DBStorage
-            class by creating
-            the engine and setting up the database connection.
-            It retrieves the
-            necessary database connection information
-            from environment variables.
-            The engine is created using the SQLAlchemy create_engine function
-            with the MySQL database URL.
-            If the HBNB_ENV environment variable is
-            set to "test", it drops all tables in the database. This method is
-            typically called when creating
-            a new instance of the DBStorage class.
-        """
-        user = getenv("HBNB_MYSQL_USER")
-        passwd = getenv("HBNB_MYSQL_PWD")
-        db = getenv("HBNB_MYSQL_DB")
-        host = getenv("HBNB_MYSQL_HOST")
-        env = getenv("HBNB_ENV")
-
-        self.__engine = create_engine(
-            "mysql+mysqldb://{}:{}@{}/{}".format(user, passwd, host, db),
-            pool_pre_ping=True,
-        )
-
-        if env == "test":
+        """Instantiate a DBStorage object"""
+        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        HBNB_ENV = getenv('HBNB_ENV')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                      format(HBNB_MYSQL_USER,
+                                             HBNB_MYSQL_PWD,
+                                             HBNB_MYSQL_HOST,
+                                             HBNB_MYSQL_DB))
+        if HBNB_ENV == "test":
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """
-        Returns a dictionary of objects based on the class name.
-
-        Parameters:
-            cls (str or class, optional): The class
-            name or class to filter objects.
-
-        Returns:
-            dict: A dictionary of objects with keys
-            in the format "<class_name>.<object_id>".
-
-        Description:
-            This method queries objects from the database based on
-            the provided class name or class.
-            If a class name (as a string) is provided, it evaluates
-            the string to get the corresponding class.
-            It then queries the objects of that class from the database and
-            adds them to the dictionary.
-            If no class is provided, it queries objects of several predefined
-            classes (State, City, User, Place, Review, Amenity).
-            The objects are added to the dictionary with keys
-            in the format "<class_name>.<object_id>".
-            The method returns the dictionary of objects.
-        """
-        dic = {}
-        if cls:
-            if type(cls) is str:
-                cls = eval(cls)
-            query = self.__session.query(cls)
-            for elem in query:
-                key = "{}.{}".format(type(elem).__name__, elem.id)
-                dic[key] = elem
-        else:
-            lista = [State, City, User, Place, Review, Amenity]
-            for clase in lista:
-                query = self.__session.query(clase)
-                for elem in query:
-                    key = "{}.{}".format(type(elem).__name__, elem.id)
-                    dic[key] = elem
-
-        return dic
+        """query on the current database session"""
+        new_dict = {}
+        for clss in classes:
+            if cls is None or cls is classes[clss] or cls is clss:
+                objs = self.__session.query(classes[clss]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    new_dict[key] = obj
+        return (new_dict)
 
     def new(self, obj):
-        """
-        Add a new element to the table.
-
-        Parameters:
-            obj: The object to be added to the table.
-
-        Returns:
-            None
-
-        Description:
-            This method adds a new object to the current session.
-            The object will be
-            inserted into the corresponding table when
-            the session is committed.
-            This method is typically used to add a new
-            element to the database.
-        """
-        try:
-            self.__session.add(obj)
-            self.__session.commit()  # Commit the changes to the database
-        except Exception as ex:
-            self.__session.rollback()  # Rollback in case of error
-            print(f"An error occurred: {ex}")
+        """add the object to the current database session"""
+        self.__session.add(obj)
 
     def save(self):
-        """
-        Save changes made in the session.
-
-        Parameters:
-            None
-
-        Returns:
-            None
-
-        Description:
-            This method commits the current session,
-            effectively saving any changes
-            made to the database.
-            Any pending changes in the session will be
-            persisted in the database.
-            This method is typically called after making
-            modifications to objects in the session
-            to ensure that the changes are
-            saved.
-        """
-        try:
-            # New variables added for demonstration purposes
-            operation = "commit"
-            status = "pending"
-
-            # Perform the commit operation
-            if operation == "commit" and status == "pending":
-                self.__session.commit()
-                status = "completed"
-        except Exception as e:
-            # Handle the exception
-            status = "failed"
-            print(f"An error occurred: {e}")
+        """commit all changes of the current database session"""
+        self.__session.commit()
 
     def delete(self, obj=None):
-        """
-        Delete an element from the table.
-
-        Parameters:
-            obj: The object to be deleted from the table. (optional)
-
-        Returns:
-            None
-
-        Description:
-            This method deletes the specified object from the table.
-            If an object is
-            provided, it will be deleted from the session.
-            If no object is provided,
-            this method does nothing.
-            This method is typically used to delete an
-            element from the database.
-        """
-        try:
-            # New variables for demonstration
-            operation = "delete"
-            status = "pending"
-
-            # Check if the object is provided
-            if obj:
-                self.session.delete(obj)
-                status = "completed"
-        except Exception as e:
-            # Handle any exceptions that might occur
-            status = "failed"
-            print(f"An error occurred during deletion: {e}")
+        """delete from the current database session obj if not None"""
+        if obj is not None:
+            self.__session.delete(obj)
 
     def reload(self):
-        """
-        Reload the configuration and create a new session.
-
-        Parameters:
-            None
-
-        Returns:
-            None
-
-        Description:
-            This method reloads the configuration by creating
-            the necessary tables
-            in the database based on the defined models.
-            It creates a new session
-            with the reconfigured engine and assigns it to
-            the instance variable.
-            The session is configured with `expire_on_commit=False` to prevent
-            objects from being expired after a commit. This method is typically
-            called when the configuration or database connection needs to be
-            reloaded or reset.
-        """
-        try:
-            # New variables for demonstration
-            status = "pending"
-
-            # Reconfigure the session
-            Base.metadata.create_all(self.__engine)
-            sec = sessionmaker(bind=self.__engine, expire_on_commit=False)
-            Session = scoped_session(sec)
-            self.__session = Session()
-
-            status = "completed"
-        except Exception as ex:
-            # Handle any exceptions that might occur
-            status = "failed"
-            print(f"An error occurred during reconfiguration: {ex}")
+        """reloads data from the database"""
+        Base.metadata.create_all(self.__engine)
+        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sess_factory)
+        self.__session = Session
 
     def close(self):
-        """
-        Close the current session.
-
-        Parameters:
-            None
-
-        Returns:
-            None
-
-        Description:
-            This method closes the current session
-            by calling the `close()` method
-            on the session object.
-            It ensures that any resources associated with
-            the session are released. This method is typically called when the
-            session is no longer needed or when cleaning up resources.
-        """
-        session_status = "closing"
-        attempt = 0
-
-        # Attempt to close the session
-        while session_status != "closed" and attempt <= 3:
-            try:
-                self.__session.close()
-                session_status = "closed"
-            except Exception as ex:
-                # Handle potential exceptions during session close
-                print(f"Attempt {attempt}: Failed to close session - {ex}")
-                attempt += 1
-
-        if session_status == "closed":
-            print("Session closed successfully.")
-        else:
-            print("Failed to close session after 3 attempts.")
+        """call remove() method on the private session attribute"""
+        self.__session.remove()
--- models/state.py
+++ models/state.py
@@ -1,54 +1,34 @@
 #!/usr/bin/python3
-"""This is the state class"""
-from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-from sqlalchemy.orm import relationship
-from sqlalchemy import Column, Integer, String
+""" holds class State"""
 import models
+from models.base_model import BaseModel, Base
 from models.city import City
-import shlex
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """This is the class for State.
+    """Representation of state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
 
-    Attributes:
-        name (str): The name of the state.
-        cities (relationship): A one-to-many relationship with City objects.
-    """
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
 
-    __tablename__ = "states"
-
-    name = Column(String(128), nullable=False)
-
-    cities = relationship("City", cascade="all, delete, delete-orphan",
-                          backref="state")
-
-    @property
-    def cities(self):
-        """Retrieve the cities associated with the current state.
-
-        Returns:
-            list: A list of City objects associated with the state.
-        """
-        try:
-            all_objects = models.storage.all()
-            city_objects = []
-            result = []
-            city_type = "City"
-
-            # Filter out objects that represent cities
-            for key in all_objects:
-                object_type = key.replace(".", " ")
-                object_type = shlex.split(object_type)
-                if object_type[0] == city_type:
-                    city_objects.append(all_objects[key])
-
-            # Find cities that belong to the current state
-            for city in city_objects:
+    if models.storage_t != "db":
+        @property
+        def cities(self):
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
                 if city.state_id == self.id:
-                    result.append(city)
-
-            return result
-        except Exception as e:
-            print(f"An error occurred: {e}")
+                    city_list.append(city)
+            return city_list
